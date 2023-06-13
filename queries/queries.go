90405bc7c9cca8bcfd5b7b1c53beceea7b9cc378
Logan Nguyen <lg.131.dev@gmail.com>
2023-06-13 17:12:04 -0500

    refactor: refactored GetBalanceQuery to new queries package

---
+++ queries/queries.go
@@ -0,0 +1,25 @@
+package queries
+
+import (
+	"hedera-dev/common"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+)
+
+// @dev retrieve account balance
+//
+// @param acountId *hedera.AccountID
+//
+// @param client *hedera.Client
+//
+// @return hedera.
+func GetBalanceQuery(accountId hedera.AccountID, hc *hedera.Client) hedera.AccountBalance {
+	// prepare query
+	query := hedera.NewAccountBalanceQuery().SetAccountID(accountId)
+
+	// Sign with client operator private key and submit the query to a Hedera network
+	accountBal, err := query.Execute(hc)
+	common.HandleException(err)
+
+	return accountBal
+}
