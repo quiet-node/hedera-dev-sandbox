d83d2042c1372ed242ef9d54f3bbb57ccb39c7f5
Logan Nguyen <lg.131.dev@gmail.com>
2023-06-13 17:21:00 -0500

    update: updated main.go

--- main.go
+++ main.go
@@ -3,30 +3,45 @@ package main
 import (
 	"hedera-dev/initializers"
 	"hedera-dev/models"
+	"hedera-dev/queries"
 	"log"
 
 	"github.com/hashgraph/hedera-sdk-go/v2"
 )
 
 var (
-	ha *models.HederaAcount
+	oha *models.HederaAcount
 	hc *hedera.Client
+	recipientAccountId hedera.AccountID 
 )
 
 func init() {
 	// init env vars
 	initializers.LoadEnvVars();
 
-	// construct HederaAcount
-	ha = initializers.InitHederaAccount()
+	// construct Operator HederaAcount
+	oha = initializers.InitHederaAccount()
 
 	// init HederaClient
-	hc = initializers.InitHederaClientForTestnet(ha)
+	hc = initializers.InitHederaClientForTestnet(oha)
+
+	// init recipientAccountId
+	recipientAccountId, _ = hedera.AccountIDFromString("0.0.14303758") // "0.0.14303758" is a random accountID created by transactions.CreateNewAccount()
 
 }
 
 func main() {
-	log.Println(hc.GetOperatorAccountID())
-	log.Println(hc.GetOperatorPublicKey())
-	
+
+	// create new account transaction
+	// transactions.CreateNewAccount(hc);
+
+	// transfer hbar transaction
+	// transactions.TransferHbar(oha.AccountId, recipientAccountId, hc)
+
+	// check balance query
+	opBal := queries.GetBalanceQuery(oha.AccountId, hc)
+	log.Println("Operator balance:", opBal.Hbars.AsTinybar(), "th")
+	reBal := queries.GetBalanceQuery(recipientAccountId, hc)
+	log.Println("Recipient balance:", reBal.Hbars.AsTinybar(), "th")
+
 }
