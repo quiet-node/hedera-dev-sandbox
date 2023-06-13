6041fe0e0f9a44e8e3d22b32a5cb0f5c7dc0ec69
Logan Nguyen <lg.131.dev@gmail.com>
2023-06-13 17:12:23 -0500

    new: added transactions.TransferHbar()

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -2,12 +2,13 @@ package transactions
 
 import (
 	"hedera-dev/common"
+	"hedera-dev/queries"
 	"log"
 
 	"github.com/hashgraph/hedera-sdk-go/v2"
 )
 
-// @dev handle creating a new account
+// @dev handle creating a new account with an initial balance of 1000 th
 func CreateNewAccount(hc *hedera.Client) {
 	// Generate new private key
 	newAccountPrivateKey, err := hedera.PrivateKeyGenerateEd25519()
@@ -16,23 +17,52 @@ func CreateNewAccount(hc *hedera.Client) {
 	// get pub key
 	newAccountPubKey := newAccountPrivateKey.PublicKey()
 
-	//Create new account and assign the pub key
-	newAccount, err := hedera.NewAccountCreateTransaction().SetKey(newAccountPubKey).SetInitialBalance(hedera.HbarFrom(1000, hedera.HbarUnits.Tinybar)).Execute(hc)
+	// prepare transaction creating new account and assign the pub key
+	createTransaction := hedera.NewAccountCreateTransaction().SetKey(newAccountPubKey).SetInitialBalance(hedera.HbarFrom(1000, hedera.HbarUnits.Tinybar))
+
+	txRes, err := createTransaction.Execute(hc)
 	common.HandleException(err)
 
-	//Request the receipt of the transaction
-	receipt, err := newAccount.GetReceipt(hc)
+	// Request the txReceipt of the transaction
+	txReceipt, err := txRes.GetReceipt(hc)
 	common.HandleException(err)
 
-	//Get the new account ID from the receipt
-	newAccountId := receipt.AccountID
+	//Get the new account ID from the txReceipt
+	newAccountId := txReceipt.AccountID
 
 	//Log the account ID
 	log.Printf("The new account ID: %v\n", newAccountId)
 
 	// verify new account balance
-	accountBal := common.GetBalanceQuery(*newAccountId, hc);
+	accountBal := queries.GetBalanceQuery(*newAccountId, hc);
 
 	//Print the balance of tinybars
 	log.Println("The account balance for the new account:", accountBal.Hbars.AsTinybar(), "th")
+}
+
+// @dev handle transfering hbar to a recipient
+// 
+// @param senderAccountId hedera.AccountID
+// 
+// @param receiverAccountId hedera.AccountID
+// 
+// @param client *hedera.Client
+func TransferHbar(senderAccountId, receiverAccountId hedera.AccountID, hc *hedera.Client) {
+	// prepare transaction
+	transferTransaction := hedera.NewTransferTransaction().
+						AddHbarTransfer(senderAccountId, hedera.HbarFrom(-1000, hedera.HbarUnits.Tinybar)).
+						AddHbarTransfer(receiverAccountId, hedera.HbarFrom(1000, hedera.HbarUnits.Tinybar))
+
+	// execute the transaction
+	txRes, err := transferTransaction.Execute(hc)
+	common.HandleException(err)
+
+	// Request the txReceipt of the transaction
+	txReceipt, err := txRes.GetReceipt(hc)
+	common.HandleException(err)
+
+	// verify if the transaction reached consensus by the network based on the status of the receipt
+	txStatus := txReceipt.Status
+	log.Println("The transfer transaction's consensus status: ", txStatus.String())
+	
 }
