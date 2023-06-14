e4aac8b17fd7b0db609967d9e188d3c874c3c652
Logan Nguyen <lg.131.dev@gmail.com>
2023-06-14 12:07:01 -0500

    fix(docs): updated account.go

--- models/account.go
+++ models/account.go
@@ -4,9 +4,9 @@ import "github.com/hashgraph/hedera-sdk-go/v2"
 
 // @notice information of the struct HederaAcount
 //
-// @param AccountId string
+// @param AccountId hedera.AccountID
 //
-// @param PrivateKey string
+// @param PrivateKey hedera.PrivateKey
 type HederaAcount struct {
 	AccountId hedera.AccountID
 	PrivateKey hedera.PrivateKey
